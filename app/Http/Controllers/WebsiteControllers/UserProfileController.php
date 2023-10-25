74b503214acd87b3f87e7c16e208edfbcecf0eda
Mohamed Said <mohamedsaid11128@gmail.com>
2023-10-25 10:46:39 +0200

    UserProfile

---
+++ app/Http/Controllers/WebsiteControllers/UserProfileController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\WebsiteControllers;
+
+use App\Http\Requests\CarRequest;
+use App\Http\Requests\CarReservationRequest;
+
+
+use App\Http\Requests\ChangePasswordRequest;
+use App\Models\Car;
+use App\Models\CarsBrand;
+use App\Models\CarsModel;
+use App\Models\Reset;
+use App\Models\User;
+use App\Models\Vendor;
+use App\Models\Tanant;
+use App\Traits\MainTrait;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class UserProfileController extends Controller
+{
+    use MainTrait;
+
+    //my profile
+    public function profile()
+    {
+        try {
+            $user = auth()->user();
+            return $this->successResponse('حجوزاتي ', $user);
+
+        } catch (\Throwable $th) {
+            return $this->errorResponse($th->getMessage());
+        }
+    }
+
+    public function user_change_password(ChangePasswordRequest $request)
+    {
+        try {
+            $data = $request->validated();
+            auth()->user()->update(['password' => $data['password']]);
+            return $this->successResponse('تم تغيير كلمة المرور بنجاح', []);
+        } catch (\Throwable $th) {
+            return $this->errorResponse($th->getMessage());
+        }
+    }
+
+    //my reservations
+    public function my_reservations()
+    {
+        try {
+            $reservations = Tanant::where('user_id', $this->user_id())->latest()->get();
+            return $this->successResponse('حجوزاتي ', $reservations);
+
+        } catch (\Throwable $th) {
+            return $this->errorResponse($th->getMessage());
+        }
+    }
+    //my single reserve
+    public function my_single_reserve($id)
+    {
+        try {
+            $reservation = Tanant::where('user_id', $this->user_id())->whereId($id)->first();
+            if(!$reservation)
+            {
+                return $this->errorResponse('الحجز غير موجود');
+            }
+            return $this->successResponse('حجوزاتي ', $reservation);
+
+        } catch (\Throwable $th) {
+            return $this->errorResponse($th->getMessage());
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WebsiteControllers\UserProfileController;
 use App\Http\Controllers\WebsiteControllers\VendorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,11 +23,21 @@ Route::get('/car/{car_id}', [CarController::class, 'get_single_car'])->name('ven
 Route::get('/get-all-cars-brands', [CarController::class, 'get_all_cars_brands'])->name('vendor.get_all_cars_brands');
 Route::get('/get-all-cars-brand-models', [CarController::class, 'get_all_cars_brand_models'])->name('vendor.get_all_brand_models');
 
-//Reserve Car
-Route::post('/reserve', [CarController::class, 'reserve_car'])->name('user.reserve_car');
 
 //Vendors In Website
 Route::get('/get-all-vendors', [VendorController::class, 'get_all_vendors'])->name('vendor.get_all_vendors');
 Route::get('/get-all-featured-vendors', [VendorController::class, 'get_all_featured_vendors'])->name('vendor.get_all_featured_vendors');
 Route::get('/get-single-featured-vendor/{id}', [VendorController::class, 'get_single_featured_vendor'])->name('vendor.get_single_featured_vendor');
 
+//Reserve Car
+Route::post('/reserve', [CarController::class, 'reserve_car'])->name('user.reserve_car');
+
+//User Profile
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    //user profile my reservations
+    Route::get('/user-profile', [UserProfileController::class, 'profile'])->name('user.profile');
+    Route::post('/user-change-password', [UserProfileController::class, 'user_change_password'])->name('user.user_change_password');
+    Route::get('/user-reservations', [UserProfileController::class, 'my_reservations'])->name('user.my_reservations');
+
+});
