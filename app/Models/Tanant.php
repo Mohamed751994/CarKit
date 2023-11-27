6560c6f1e1bb416a35164fbc686ece890000ada7
Mohamed Said <mohamedsaid11128@gmail.com>
2023-11-27 16:10:03 +0200

    reservation edit

--- app/Models/Tanant.php
+++ app/Models/Tanant.php
@@ -17,7 +17,7 @@ class Tanant extends Model
 
     protected $casts = [
         'car_details' => 'json',
-        'car_features'=>'json'
+        'car_features'=>'array'
     ];
 
     public function car()
