df51f8bf71b3cf9348441e832836f14107f3c263
Mohamed Said <mohamedsaid11128@gmail.com>
2023-11-28 12:08:05 +0200

    reservation car features

--- app/Http/Controllers/API/WebsiteControllers/CarController.php
+++ app/Http/Controllers/API/WebsiteControllers/CarController.php
@@ -141,15 +141,7 @@ class CarController extends Controller
             $data['total_amount_after_discount'] = $this->get_total_amount_after_discount($data['total_amount'], $data['discount_percentage']);
             $data['vendor_user_id'] = $car->user_id;
             $data['car_details'] = json_encode($car);
-            if(isset($data['car_features']) && count($data['car_features']) > 0)
-            {
-                $features = [];
-                foreach ($data['car_features'] as $feature)
-                {
-                    array_push($features , $feature);
-                }
-                $data['car_features'] = $features;
-            }
+            $data['car_features'] = $data['car_features'] ? json_encode($data['car_features']) : null;
 
             $tanant = Tanant::create($data);
 
